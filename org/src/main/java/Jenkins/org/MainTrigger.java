30340bfbed2cf50d84f6bea81324c42058db2ef4
Shashi <Shashi@10.0.0.34>
2019-12-10 23:25:35 +0530

    2

--- org/src/main/java/Jenkins/org/MainTrigger.java
+++ org/src/main/java/Jenkins/org/MainTrigger.java
@@ -1,9 +1,7 @@
 package Jenkins.org;
 
-import org.openqa.selenium.By;		
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;		
 import org.testng.Assert;		
 import org.testng.annotations.Test;	
 import org.testng.annotations.BeforeTest;	
